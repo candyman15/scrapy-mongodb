c745feacafae6a917dd52246bbd00501361184e9
박동혁 <dhpark@ysmcorp.com>
2016-06-03 16:28:45 +0900

    Initial commit

---
+++ crawl/spiders/spider.py
@@ -0,0 +1,33 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from scrapy.item import Item, Field
+
+
+class CrawlItem(Item):
+    stock_id = Field();
+    stock_name = Field();
+
+
+class StockSpider(BaseSpider):
+    name = "stock"
+    allowed_domains = ["finance.daum.net"]
+    start_urls = [
+        "http://finance.daum.net/quote/all.daum?type=U&stype=P",
+        "http://finance.daum.net/quote/all.daum?type=U&stype=Q",
+    ]
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+
+        sites = hxs.select('//table[@class="gTable clr"]')
+
+        items = []
+
+        for site in sites:
+            item = CrawlItem()
+            href = site.select('//td[@class="txt"]/a/@href').extract()
+            item['stock_id'] = [i.split('=')[1] for i in href]
+            item['stock_name'] = site.select('//td[@class="txt"]/a//text()').extract()
+            items.append(item)
+
+        return items
